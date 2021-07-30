d69690c4dd539a7681a6f7b15e4854742f734781
cmungall <cjm@berkeleybop.org>
2021-07-29 17:40:40 -0700

    CLI

--- README.md
+++ README.md
@@ -3,3 +3,27 @@
 Extension to linkml-runtime for converting between instances of LinkML models and CSVs. This may involve selective normalization/denormalization, plus serialization of selected elements as JSON/YAML
 
 This builds on [json-flattener](https://github.com/cmungall/json-flattener)
+
+## Command Line Usage
+
+Denormalizing conversion from YAML instance data to TSV
+
+```
+link-convert \
+  -s examples/bookshop.schema.yaml \
+  -C Shop \
+  -S all_book_series \
+  -o examples/shop1.instance.tsv \
+  examples/shop1.instance.yaml
+```
+
+Converting back to YAML/JSON:
+
+```
+link-convert \
+  -s examples/bookshop.schema.yaml \
+  -C Shop \
+  -S all_book_series \
+  -o examples/shop1-troundtrip.instance.json \
+  examples/shop1.instance.tsv
+```
--- linkml_csv/utils/converter.py
+++ linkml_csv/utils/converter.py
@@ -1,31 +1,87 @@
 import os
 import re
 import sys
+from types import ModuleType
 
 import click
 
+from linkml.generators.yamlgen import YAMLGenerator
+from linkml.generators.pythongen import PythonGenerator
+from linkml_runtime.utils.compile_python import compile_python
 
-def compare_files(file: str, target: str, comments: str = r'^\s+#.*\n') -> int:
-    def filtr(txt: str) -> str:
-        return re.sub(comments, '', txt, flags=re.MULTILINE).strip()
-    if os.path.exists(target):
-        with open(target) as oldfile:
-            oldtext = filtr(oldfile.read())
-    else:
-        oldtext = ""
+from linkml_csv.dumpers.csv_dumper import CSVDumper
+from linkml_runtime.dumpers.yaml_dumper import YAMLDumper
+from linkml_runtime.dumpers.json_dumper import JSONDumper
+from linkml_runtime.dumpers.dumper_root import Dumper
+from linkml_csv.loaders.csv_loader import CSVLoader
+from linkml_runtime.loaders.yaml_loader import YAMLLoader
+from linkml_runtime.loaders.json_loader import JSONLoader
+from linkml_runtime.loaders.loader_root import Loader
 
-    with open(file) as newfile:
-        newtext = filtr(newfile.read())
-    return int(oldtext == newtext)
+dumpers_loaders = {
+    'tsv': (CSVDumper, CSVLoader),
+    'csv': (CSVDumper, CSVLoader),
+    'yaml': (YAMLDumper, YAMLLoader),
+    'json': (JSONDumper, JSONLoader),
+}
+
+def make_python(schema) -> ModuleType:
+    """
+    Note: if you change the yaml schema and associated test instance objects,
+    you may need to run this test twice
+    """
+    pstr = str(PythonGenerator(schema, mergeimports=True).serialize())
+    m = compile_python(pstr)
+    return m
+
+def _get_format(input: str, input_format: str =None):
+    if input_format is None:
+        _, ext = os.path.splitext(input)
+        if ext is not None:
+            input_format = ext.replace('.', '')
+        else:
+            raise Exception(f'Must pass format option OR use known file suffix: {input}')
+    return input_format.lower()
+
+def _is_xsv(fmt: str) -> bool:
+    return fmt == 'csv' or fmt == 'tsv'
+
+def get_loader(fmt: str) -> Loader:
+    return dumpers_loaders[fmt][1]()
+def get_dumper(fmt: str) -> Loader:
+    return dumpers_loaders[fmt][0]()
 
 
 @click.command()
-@click.argument("file1", type=click.Path(exists=True, dir_okay=False))
-@click.argument("file2", type=click.Path(dir_okay=False))
-@click.option("-c", "--comments", help="Comments regexp", default="^#.*$", show_default=True)
-def cli(file1, file2, comments) -> None:
-    """ Compare file1 to file2 using a filter """
-    sys.exit(compare_files(file1, file2, comments))
+@click.option("--output", "-o")
+@click.option("--input-format", "-f")
+@click.option("--output-format", "-t")
+@click.option("--schema", "-s")
+@click.option("--index-slot", "-S", required=True)
+@click.option("--target-class", "-C")
+@click.argument("input")
+def cli(input, output=None, input_format=None, output_format=None, index_slot=None, schema=None, target_class=None) -> None:
+    """
+    Converts to/from TSV to rich LinkML instance format (JSON/YAML/RDF)
+    """
+    print(f'IN={input}')
+    python_module = make_python(schema)
+    target_class = python_module.__dict__[target_class]
+    schema = YAMLGenerator(schema).schema
+    input_format = _get_format(input, input_format)
+    output_format = _get_format(output, output_format)
+    loader = get_loader(input_format)
+    dumper = get_dumper(output_format)
+
+    if _is_xsv(input_format):
+        obj = loader.load(source=input, target_class=target_class, schema=schema, index_slot=index_slot)
+    else:
+        obj = loader.load(source=input,  target_class=target_class)
+    print(f'Obj={obj}')
+    if _is_xsv(output_format):
+        obj = dumper.dump(obj, output, schema=schema, index_slot=index_slot)
+    else:
+        obj = dumper.dump(obj, output)
 
 
 if __name__ == '__main__':
